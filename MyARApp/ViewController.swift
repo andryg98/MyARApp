1537141d648cca4d300aba45233a78067c26424f
Andrea Garau <andreagarau98@gmail.com>
2018-09-21 16:52:40 +0200

    Added currentFrame at 10 cm from the camera

--- MyARApp/ViewController.swift
+++ MyARApp/ViewController.swift
@@ -11,65 +11,91 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+  
+  @IBOutlet var sceneView: ARSCNView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+    // Set the view's delegate
+    sceneView.delegate = self
+    
+    // Show statistics such as fps and timing information
+    sceneView.showsStatistics = true
+    
+    // Create a new scene
+    let scene = SCNScene(named: "art.scnassets/ship.scn")!
+    
+    // Set the scene to the view
+    sceneView.scene = scene
+    
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(gestureRecognize:)))
+    view.addGestureRecognizer(tapGesture)
+  }
+  
+  @objc func handleTap(gestureRecognize: UITapGestureRecognizer) {
+    guard let currentFrame = sceneView.session.currentFrame else {
+      return
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
+    // Create an image plane using a snapshot of the view
+    let imagePlane = SCNPlane(width: sceneView.bounds.width / 6000, height: sceneView.bounds.height / 6000)
+    imagePlane.firstMaterial?.diffuse.contents = sceneView.snapshot()
+    imagePlane.firstMaterial?.lightingModel = .constant
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
+    //Create a plane node and add it to the scene
+    /*
+      The object is placed in position (0,0,0).
+     */
+    let planeNode = SCNNode(geometry: imagePlane)
+    sceneView.scene.rootNode.addChildNode(planeNode)
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
+    // Set transform of node to be 10cm in front of camera
+    var translation = matrix_identity_float4x4
+    translation.columns.3.z = -0.1
+    planeNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
+    // Create a session configuration
+    let configuration = ARWorldTrackingConfiguration()
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
+    // Run the view's session
+    sceneView.session.run(configuration)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+    // Pause the view's session
+    sceneView.session.pause()
+  }
+  
+  // MARK: - ARSCNViewDelegate
+  
+  /*
+   // Override to create and configure nodes for anchors added to the view's session.
+   func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+   let node = SCNNode()
+   
+   return node
+   }
+   */
+  
+  func session(_ session: ARSession, didFailWithError error: Error) {
+    // Present an error message to the user
+    
+  }
+  
+  func sessionWasInterrupted(_ session: ARSession) {
+    // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    
+  }
+  
+  func sessionInterruptionEnded(_ session: ARSession) {
+    // Reset tracking and/or remove existing anchors if consistent tracking is required
+    
+  }
 }
